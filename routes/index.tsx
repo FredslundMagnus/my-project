075fc94a32497dcc38c1019d1d9ab117d9f22ae2
Magnus Fredslund <fredslundmagnus@gmail.com>
2022-07-09 13:22:56 +0200

    asd

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,30 +1,27 @@
 /** @jsx h */
-import { h } from "preact";
+import { Fragment, h } from "preact";
+import { Head } from "$fresh/runtime.ts";
 import { tw } from "@twind";
 import Counter from "../islands/Counter.tsx";
 
 export default function Home() {
   return (
-    <html>
-      <div>
-        <head>
-          <title>Testing 2</title>
-        </head>
+    <Fragment>
+      <Head>
+        <title>Testing 3</title>
+      </Head>
+      <div class={tw`p-4 mx-auto max-w-screen-md`}>
+        <img
+          src="/logo.svg"
+          height="100px"
+          alt="the fresh logo: a sliced lemon dripping with juice"
+        />
+        <p class={tw`my-6`}>
+          Welcome to `fresh`. Try update this great message in the
+          ./routes/index.tsx file, and refresh.
+        </p>
+        <Counter start={3} />
       </div>
-      <body>
-        <div class={tw`p-4 mx-auto max-w-screen-md`}>
-          <img
-            src="/logo.svg"
-            height="100px"
-            alt="the fresh logo: a sliced lemon dripping with juice"
-          />
-          <p class={tw`my-6`}>
-            Welcome to `fresh`. Try update this great message in the
-            ./routes/index.tsx file, and refresh.
-          </p>
-          <Counter start={3} />
-        </div>
-      </body>
-    </html>
+    </Fragment>
   );
 }

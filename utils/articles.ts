5483391c6e85cf95ccaceb7f1f1bb2be1d1fb986
Magnus Fredslund <fredslundmagnus@gmail.com>
2022-07-10 15:15:08 +0200

    asd

--- utils/articles.ts
+++ utils/articles.ts
@@ -1,7 +1,12 @@
-export function articleNames(): string[] {
-  const articles = Array.from(Deno.readDirSync("./articles")).filter((entry) =>
-    entry.name.endsWith(".md")
-  ).map((entry) => entry.name.replace(".md", ""));
+export async function articleNames(): Promise<string[]> {
+  const articles: string[] = [];
+  for await (const dirEntry of Deno.readDir("./articles")) {
+    articles.push(dirEntry.name.replace(".md", ""));
+  }
+
+  //   const articles = Array.from(Deno.readDir("./articles").map(())).filter((entry) =>
+  //     entry.name.endsWith(".md")
+  //   ).map((entry) => entry.name.replace(".md", ""));
   console.log(articles);
   return articles;
 }

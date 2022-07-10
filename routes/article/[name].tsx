e2d74dfb3e75e8c2443183580126d1de90aca016
Magnus Fredslund <fredslundmagnus@gmail.com>
2022-07-10 22:16:15 +0200

    df

--- routes/article/[name].tsx
+++ routes/article/[name].tsx
@@ -69,14 +69,20 @@ export default function Article({ data }: PageProps<Markdown | null>) {
         {/* <meta property="article:expiration_time" content="2013-09-16T19:08:47+01:00" /> */}
         <meta property="article:author" content={data.author} />
         <meta property="article:section" content="Article Section" />
-        <meta property="article:tag" content="Article Tag" />
+        {/* <meta property="article:tag" content="Article Tag" /> */}
+        {data.tags.map((tag) => {
+          return <meta property="article:tag" content={tag} />;
+        })}
 
         <meta property="og:article:published_time" content={data.date} />
         {/* <meta property="og:article:modified_time" content="2013-09-16T19:08:47+01:00" /> */}
         {/* <meta property="og:article:expiration_time" content="2013-09-16T19:08:47+01:00" /> */}
         <meta property="og:article:author" content={data.author} />
         <meta property="og:article:section" content="Article Section" />
-        <meta property="og:article:tag" content="Article Tag" />
+        {/* <meta property="og:article:tag" content="Article Tag" /> */}
+        {data.tags.map((tag) => {
+          return <meta property="og:article:tag" content={tag} />;
+        })}
       </Head>
       <article class={article}>
         <h2 class={tags}>{data.tags.map((s) => s.toUpperCase()).join(", ")}</h2>
